df6f1e50fef0e4d4011b1006fe05e892cbe64fdf
aryan <aryasangwan00@gmail.com>
2022-05-09 09:55:33 +0530

    last_login add to display

--- todo/admin.py
+++ todo/admin.py
@@ -7,7 +7,7 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('id','email', 'name','lastname', 'is_admin','is_active' ,'created_at','update_at')
+    list_display = ('id','email', 'name','lastname', 'is_admin','is_active','last_login' ,'created_at','update_at')
     list_filter = ('is_admin',)
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
